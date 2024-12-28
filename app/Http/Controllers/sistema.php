211f3bdc7a7d15c81b48ca232c567e75f2ee0bdc
Luc1795 <lunahernandezbeltran95@hotmail.com>
2024-12-27 19:39:57 -0600

    primer commit

---
+++ app/Http/Controllers/sistema.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\sistema;
+use Illuminate\Http\Request;
+
+class sistema extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(sistema $sistema)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(sistema $sistema)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, sistema $sistema)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(sistema $sistema)
+    {
+        //
+    }
+}
